64f453e9f3f9f29592d9d91aa8248f2adaa9e889
Danny <hydanny@yahoo.com>
2017-04-06 00:50:48 -0700

    Style about page & link to websites

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,8 +3,24 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
-      <p>Welcome to the about page.</p>
+      <h1 className="text-center">About</h1>
+      <p>
+        Just a basic weather application for you lazy bums.
+      </p>
+      <p>
+        Don't care about the weather?  Here are some external links you might find useful:
+      </p>
+      <ol>
+        <li>
+          <a href="https://reddit.com">Reddit</a> - News, memes, etc.
+        </li>
+        <li>
+          <a href="https://google.com">Google</a> - Google is your best friend.
+        </li>
+        <li>
+          <a href="https://slickdeals.net">Slickdeals</a> - Who doesn't like sales?
+        </li>
+      </ol>
     </div>
   )
 };
