15a56630f554fcbac7d30352b2e8bc10b7f380ec
Danny <hydanny@yahoo.com>
2017-04-11 23:33:29 -0700

    Added css style

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,7 @@ var React = require('react');
 var About = (props) => {
   return (
     <div>
-      <h1 className="text-center">About</h1>
+      <h1 className="text-center page-title">About</h1>
       <p>
         Just a basic weather application for you lazy bums.
       </p>
--- app/components/WeatherForm.jsx
+++ app/components/WeatherForm.jsx
@@ -15,7 +15,7 @@ var WeatherForm = React.createClass({
     return (
         <div>
           <form onSubmit={this.onFormSubmit}>
-            <input type="text" ref="location" placeholder="Enter City Name"/>
+            <input type="search" ref="location" placeholder="Enter City Name"/>
             <button className="button expanded hollow">Weather pls</button>
           </form>
         </div>
